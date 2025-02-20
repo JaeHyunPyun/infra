d1751f6d42c0e89f2d2abef884ca553734a6bd83
jaehyunpyun <pyunjaehyun@gmail.com>
2025-02-20 12:49:55 +0900

    feat: add ci,cd pipeline

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM openjdk:17-jdk-slim
 WORKDIR /app
 
-COPY build/libs/demo-0.0.1-SNAPSHOT.jar app.jar
+COPY build/libs/*SNAPSHOT.jar app.jar
 
 CMD ["java", "-jar", "app.jar"]
---
+++ src/main/java/com/example/infra/AppController.java
@@ -0,0 +1,41 @@
+package com.example.infra;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AppController {
+
+    @Autowired
+    private RedisTemplate<String, Object> redisTemplate;
+
+    @GetMapping("/redis")
+    public String home() {
+        // Redis에 값 저장
+        redisTemplate.opsForValue().set("cdf", "def");
+
+        // Redis에서 값 가져오기
+        String value = (String)redisTemplate.opsForValue().get("cdf");
+
+        return "Value from Redis: " + value;
+    }
+
+    @GetMapping("/")
+    public String hello() {
+        return "checking ci pipeline";
+    }
+
+    @GetMapping("/api/login")
+    public String login() {
+        return "login connected!";
+    }
+
+    @GetMapping("/search")
+    public String search() {
+        return "search connected!";
+    }
+
+}
+
