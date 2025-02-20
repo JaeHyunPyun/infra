24172de46d8627a80a89b3e66a81ffb0967d9d68
juyeon6069 <jynam6069@gmail.com>
2025-02-20 16:33:21 +0900

    test failed

--- src/test/java/com/example/infra/DefaultTest.java
+++ src/test/java/com/example/infra/DefaultTest.java
@@ -8,6 +8,6 @@ public class DefaultTest {
     @Test
     void testAlwaysFail() {
         // 실패하는 조건을 만들기 위해 거짓 값을 assert
-        assertTrue(false, "This test will always fail");
+        assertTrue(false, "This test will");
     }
 }
