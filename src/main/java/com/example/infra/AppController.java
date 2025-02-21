541a46a608f7bd2204e950b7607d4d156709d132
jaehyunpyun <pyunjaehyun@gmail.com>
2025-02-22 01:47:16 +0900

    add:  adding database connection code

--- src/main/java/com/example/infra/AppController.java
+++ src/main/java/com/example/infra/AppController.java
@@ -1,5 +1,10 @@
 package com.example.infra;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +16,9 @@ public class AppController {
     @Autowired
     private RedisTemplate<String, Object> redisTemplate;
 
+    @Autowired
+    private DataSource dataSource;
+
     @GetMapping("/redis")
     public String home() {
         // Redis에 값 저장
@@ -21,6 +29,32 @@ public class AppController {
 
         return "Value from Redis: " + value;
     }
+
+    @GetMapping("/db")
+    public String getDb() {
+        String deptName = "";
+        String deptno = "";
+        String loc ="";
+        try (Connection connection = dataSource.getConnection()) {
+            // SQL 쿼리 실행
+            String sql = "SELECT deptno, dname, loc FROM dept2 WHERE deptno = 30";
+            try (Statement statement = connection.createStatement();
+                ResultSet resultSet = statement.executeQuery(sql)) {
+
+                // 결과 처리
+                if (resultSet.next()) {
+                    deptno = resultSet.getString("deptno");
+                    deptName = resultSet.getString("dname");
+                    loc = resultSet.getString("loc");
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return "Value from DB: " + deptno +" "+ deptName+" "+loc;
+    }
+
 
     @GetMapping("/")
     public String hello() {
