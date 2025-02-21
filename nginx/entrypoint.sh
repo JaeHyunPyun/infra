94ce9d03a1b6499c1c24147da610b156ea97aeea
jaehyunpyun <pyunjaehyun@gmail.com>
2025-02-21 12:07:51 +0900

    fix: change domain

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # certbot을 사용해 SSL 인증서 자동 발급
-certbot --nginx -d purpleturtle.p-e.kr --email pyunjaehyun@gmail.com --agree-tos --no-eff-email
+certbot --nginx -d greenturtle.p-e.kr --email pyunjaehyun@gmail.com --agree-tos --no-eff-email
 
 # certbot 후 nginx를 재시작하여 계속 포그라운드에서 실행되도록 설정
 nginx -s reload
